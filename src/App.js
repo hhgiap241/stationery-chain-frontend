27d2877d0398ff2da5fd46f84d155f6187adf578
Hoàng Hữu Giáp <69495243+hhgiap241@users.noreply.github.com>
2022-08-14 13:45:57 +0700

    Init login function (#1)
    Co-authored-by: giaphoang <hhgiap241>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Keycloak from "keycloak-js";
+import {Button} from "react-bootstrap";
 
+const initOptions = {
+    url: "http://localhost:8180/auth/",
+    realm: "stationery-realm",
+    clientId: "react-client",
+    onLoad: "login-required",
+}
+const keycloak = new Keycloak(initOptions);
+keycloak.init({onLoad: initOptions.onLoad})
+    .then((authenticated) => {
+        alert(authenticated ? 'authenticated' : 'not authenticated');
+        localStorage.setItem('accessToken', keycloak.token);
+        localStorage.setItem('refreshToken', keycloak.refreshToken);
+        console.log(keycloak.token);
+    }).catch(err => {
+    console.log(err);
+    alert('failed to authenticated');
+})
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Button variant="primary" onClick={() => keycloak.login()}>Login</Button>
+            <Button variant="primary" onClick={() => keycloak.logout()}>Logout</Button>
+        </div>
+    )
 }
 
 export default App;
