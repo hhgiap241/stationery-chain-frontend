f6574aea477bfd5245a362dc07b4c69e09cf06c1
giaphoang <hhgiap241>
2022-09-12 16:02:18 +0700

    Complete cart page.

--- src/components/customer/CartItem.js
+++ src/components/customer/CartItem.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import HttpService from "../../services/HttpService";
+import {Button} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 const CartItem = React.memo(({cartItem, totalPrice, onPriceChange}) => {
     console.log(cartItem, totalPrice);
     const [defaultProduct, setDefaultProduct] = useState({name: '', price: 0, url: ''});
     const [product, setProduct] = useState(cartItem);
+    const navigate = useNavigate();
     useEffect(() => {
         HttpService.getAxiosInstance().get(`http://localhost:8080/api/v1/product/${product.skuCode}`)
             .then(res => {
@@ -43,16 +46,34 @@ const CartItem = React.memo(({cartItem, totalPrice, onPriceChange}) => {
             console.log(err);
         });
     }
+    const handleRemoveCartItemBtn = () => {
+        const cartItem = {
+            id: product.id,
+            quantity: 0,
+            price: 0,
+            skuCode: product.skuCode,
+        };
+        console.log(cartItem);
+        const userId = localStorage.getItem('user_id');
+        HttpService.getAxiosInstance().put(`http://localhost:8080/api/v1/cart/${userId}`, cartItem)
+            .then(res => {
+                console.log('Deleted');
+                console.log(res.data);
+                navigate(0);
+            }).catch(err => {
+            console.log(err);
+        });
+    }
     return (
         <tr>
             <td>
                 <div className={'cart-info'}>
                     <img src={defaultProduct.url} alt={defaultProduct.name}/>
                     <div>
-                        <p>{defaultProduct.name}</p>
+                        <p style={{fontSize: '20px', marginBottom: '0px'}}>{defaultProduct.name}</p>
                         <small>Unit Price: {defaultProduct.price}$</small>
                         <br/>
-                        <a href={'ddfsdfsdfs'}>Remove</a>
+                        <Button variant={'outline-danger'} style={{padding: '3px'}} onClick={handleRemoveCartItemBtn}>Remove</Button>
                     </div>
                 </div>
             </td>
